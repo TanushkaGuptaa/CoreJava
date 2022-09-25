f06d2f4c39b14397aca7f9515d6ca75ff69cbe54
Tanushka Gupta <111428300+TanushkaGuptaa@users.noreply.github.com>
2022-09-25 14:52:16 +0530

    Add files via upload

---
+++ ifelseLadder.java
@@ -0,0 +1,47 @@
+/*Program :- if else ladder demo2
+@author:- Tanushka Gupta
+@date - 9-Sep-2022 
+*/
+//Java Program for implementation of if-else-if statement
+import java.util.Scanner;
+class Ifelse
+{
+	//declaring main method
+     public static void main(String []args)
+     {
+         //Take input from the user
+         //Create an instance of the Scanner class
+         Scanner sc=new Scanner(System.in);
+         System.out.println("Enter the marks obtained: ");
+         int num=sc.nextInt();
+         //Determine the grades from the marks obtained
+          if(num<50)
+          {  
+                System.out.println("fail");  
+          }  
+          else if(num>=50 && num<60)
+          {  
+                System.out.println("D grade");  
+          }  
+          else if(num>=60 && num<70)
+          {  
+               System.out.println("C grade");  
+          }  
+          else if(num>=70 && num<80)
+          {  
+               System.out.println("B grade");  
+          }  
+          else if(num>=80 && num<90)
+          {  
+              System.out.println("A grade");  
+          }
+          else if(num>=90 && num<100)
+          {  
+              System.out.println("A+ grade");  
+          }
+          else
+          {  
+             System.out.println("Invalid!");  
+          }  
+     }
+}
